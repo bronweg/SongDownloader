226b598e68a3895f58b4ba9b27cd0d874082ec1d
betty <uvetki@gmail.com>
2024-03-30 18:31:02 +0300

    disable video postprocess

--- downloader.py
+++ downloader.py
@@ -134,10 +134,9 @@ def prepare_subprocess(youtube_url, audio_only, output_path, progress_path):
     else:
         cmd.extend([
             '--format', 'mp4',
-            '--format-sort', 'codec:h264',
-            '--use-postprocessor', 'FFmpegCopyStream',
+            # '--format-sort', 'codec:h265',
+            # '--use-postprocessor', 'FFmpegCopyStream',
             # '--postprocessor-args', "CopyStream: -c:a aac -c:v libx265 -tag:v hvc1",
-            '--postprocessor-args', "CopyStream: -c:a aac -c:v libx264",
         ])
 
     cmd.extend([
